0fdd31101b5d6273f5670943eeae324a75605c3e
haddjs <nadirhexonyte@gmail.com>
2025-09-01 01:00:04 +0700

    new feature: Grouping for list in v1.2.0

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,89 +2,170 @@
 
 import useLocalStorage from "@/hooks/useLocalStorage";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Checkbox } from "./ui/checkbox";
+import { Card, CardHeader, CardContent } from "./ui/card";
+import {
+	Dialog,
+	DialogTrigger,
+	DialogHeader,
+	DialogTitle,
+	DialogContent,
+	DialogClose,
+} from "./ui/dialog";
 
 const ToDoList = () => {
-	const [todos, setTodos] = useLocalStorage("list-it-todos", []);
-	const [newTodo, setNewTodo] = useState("");
+	const [groupTitle, setGroupTitle] = useState("");
+	const [groups, setGroups] = useLocalStorage("list-it-todos", []);
+	const [groupInput, setGroupInput] = useState({});
+	const [date, setDate] = useState(null);
+	const [idGroup, setIdGroup] = useState(1);
+	const [idTodos, setIdTodos] = useState(1);
 
-	const handleNewTodo = (e) => {
+	useEffect(() => {
+		setDate(Date.now());
+	}, []);
+
+	const handleNewGroup = (e) => {
 		e.preventDefault();
+		if (groupTitle.trim() === "") return;
 
-		if (newTodo.trim() === "") return;
+		const newGroup = {
+			id: idGroup,
+			dateGroup: date,
+			title: groupTitle,
+			todos: [],
+			newTask: "",
+		};
+
+		setGroups([newGroup, ...groups]);
+		setGroupTitle("");
+
+		setIdGroup((prev) => prev + 1);
+	};
+
+	const addToGroup = (groupId) => {
+		const taskContent = groupInput[groupId] || "";
+
+		if (taskContent.trim() === "") return;
 
 		const newTask = {
-			id: Date.now(),
-			task: newTodo,
+			id: idTodos,
+			task: taskContent,
 			isComplete: false,
 		};
 
-		setTodos([newTask, ...todos]);
-		setNewTodo("");
-	};
-
-	const handleChangeTodo = (inputTask) => {
-		setNewTodo(inputTask);
-	};
-
-	const handleDeleteTodo = (deleteId) => {
-		const deletedList = todos.filter((todo) => todo.id !== deleteId);
-		setTodos(deletedList);
-	};
-
-	const handleUpdateTodo = (updateId) => {
-		const updateTodo = todos.map((todo) => {
-			if (todo.id !== updateId) return todo;
-
-			return { ...todo, isComplete: !todo.isComplete };
+		setGroups((prevGroups) => {
+			return prevGroups.map((group) => {
+				if (group.id !== groupId) return group;
+				return { ...group, todos: [...group.todos, newTask] };
+			});
 		});
 
-		setTodos(updateTodo);
-		console.log(updateTodo);
+		setGroupInput((prev) => ({
+			...prev,
+			[groupId]: "",
+		}));
+		setIdTodos((prev) => prev + 1);
+	};
+
+	const handleChangeGroup = (inputGroup) => {
+		setGroupTitle(inputGroup);
+	};
+
+	const handleGroupInput = (inputValue, groupId) => {
+		setGroupInput((prev) => ({
+			...prev,
+			[groupId]: inputValue,
+		}));
+		console.log("handling id:", groupId);
+	};
+
+	const handleDeleteTodo = (groupId, deleteListId) => {
+		setGroups((prevGroups) =>
+			prevGroups.map((group) => {
+				if (group.id !== groupId) return group;
+
+				return {
+					...group,
+					todos: group.todos.filter((todo) => todo.id !== deleteListId),
+				};
+			})
+		);
 	};
 
 	return (
-		<div>
+		<>
 			<div className="flex">
-				<form onSubmit={handleNewTodo} className="flex gap-2">
-					<Input
-						type="text"
-						value={newTodo}
-						onChange={(e) => handleChangeTodo(e.target.value)}
-						placeholder="What's new Today?"
-						className="border-2 rounded-lg"
-						required
-					/>
-					<Button type="submit" className="cursor-pointer">
-						Add
-					</Button>
-				</form>
+				<Dialog>
+					<DialogTrigger asChild>
+						<Button variant="default" className="cursor-pointer">
+							Add New Group
+						</Button>
+					</DialogTrigger>
+					<DialogContent>
+						<form onSubmit={handleNewGroup} className="flex flex-col gap-2">
+							<DialogHeader>
+								<DialogTitle>New Task</DialogTitle>
+							</DialogHeader>
+							<Input
+								type="text"
+								value={groupTitle}
+								onChange={(e) => handleChangeGroup(e.target.value)}
+								placeholder="Make New Group"
+								className="border-2 rounded-lg my-2"
+								required
+							/>
+							<DialogClose asChild>
+								<Button type="submit" className="cursor-pointer">
+									Add Group
+								</Button>
+							</DialogClose>
+						</form>
+					</DialogContent>
+				</Dialog>
 			</div>
 
-			<div className="py-4">
-				{todos.map((item) => (
-					<div key={item.id} className="flex items-center gap-2 py-2">
-						<span className={item.isComplete ? "line-through" : ""}>
-							{item.task}
-						</span>
-						<Button
-							onClick={() => handleDeleteTodo(item.id)}
-							variant="secondary"
-							size="icon"
-							className="cursor-pointer">
-							❌
-						</Button>
-						<Checkbox
-							checked={item.isComplete}
-							onCheckedChange={() => handleUpdateTodo(item.id)}
-						/>
-					</div>
+			<div className="grid grid-cols-4 justify-center py-5 gap-4">
+				{groups.map((item) => (
+					<Card key={item.id}>
+						<CardHeader>{item.title}</CardHeader>
+						<CardContent>
+							<form
+								onSubmit={(e) => {
+									e.preventDefault();
+									addToGroup(item.id);
+								}}
+								className="flex gap-2">
+								<Input
+									type="text"
+									value={groupInput[item.id] || ""}
+									onChange={(e) => handleGroupInput(e.target.value, item.id)}
+									placeholder={`Add a task to ${item.title}`}
+									required
+								/>
+								<Button type="submit">Add</Button>
+							</form>
+
+							{item.todos?.map((todo) => (
+								<div key={todo.id} className="py-4">
+									<p className="p-4 rounded-xl bg-zinc-200 text-xl break-words whitespace-normal flex justify-between items-center">
+										{todo.task}
+										<Button
+											onClick={() => handleDeleteTodo(item.id, todo.id)}
+											className="cursor-pointer">
+											X
+										</Button>
+									</p>
+								</div>
+							))}
+						</CardContent>
+					</Card>
 				))}
 			</div>
-		</div>
+		</>
 	);
 };
 
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,18 +4,20 @@ const useLocalStorage = (key, initialValue) => {
 	const [value, setValue] = useState(initialValue);
 
 	useEffect(() => {
+		if (typeof window === "undefined") return;
 		const stored = localStorage.getItem(key);
+
 		if (stored) {
 			setValue(JSON.parse(stored));
 		}
 	}, [key]);
 
-	const setStoredValue = (newValue) => {
-		setValue(newValue);
-		localStorage.setItem(key, JSON.stringify(newValue));
-	};
+	useEffect(() => {
+		if (typeof window === "undefined") return;
+		localStorage.setItem(key, JSON.stringify(value));
+	}, [key, value]);
 
-	return [value, setStoredValue];
+	return [value, setValue];
 };
 
 export default useLocalStorage;
